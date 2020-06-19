31a598b6c97ed9b3057261353944d0ce64fff01e
Theresa E <t.eatherly@gmail.com>
2020-06-19 17:28:18 -0500

    name.js debug

--- src/components/name.js
+++ src/components/name.js
@@ -13,31 +13,4 @@ class Name extends React.Component {
     this.setState({ count: this.state.count + 1 });
   };
 
-  // handleDecrement decreases this.state.count by 1
-  handleDecrement = () => {
-    // We always use the setState method to update a component's state
-    this.setState({ count: this.state.count - 1 });
-  };
-
-  // The render method returns the JSX that should be rendered
-  render() {
-    return (
-      <div className="card text-center">
-        <div className="card-header bg-primary text-white">
-          Click Counter!
-        </div>
-        <div className="card-body">
-          <p className="card-text">Click Count: {this.state.count}</p>
-          <button className="btn btn-primary" onClick={this.handleIncrement}>
-            Increment
-          </button>{" "}
-          <button className="btn btn-danger" onClick={this.handleDecrement}>
-            Decrement
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Counter;
+  
