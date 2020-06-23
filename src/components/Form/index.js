72e708308ce9374a970a435d9696d6b9f24358ed
Theresa E <t.eatherly@gmail.com>
2020-06-23 17:06:33 -0500

    Form.index.js - Cleaned

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,31 +1,21 @@
 import React from "react";
 import "./style.css";
 
-// class Form extends Component {
-  export default function Form (props) {
-
-  
-  // constructor(props){
-  //   super(props);
-  //   this.state = {search: ""}
-  // };
-      //  render() {
-     return (
-       <div>
-           <form className="form">
-          <input
-            value={props.search}
-            // {this.state.firstName || this.state.lastName}
-            name="search"
-            onChange={props.handleInputChange}
-             type="text"
-            placeholder="Search"
-          />
-             </form>
+export default function Form(props) {
+  return (
+    <div>
+      <form className="form">
+        <input
+          // This searches the value of {this.state.firstName || this.state.lastName}
+          value={props.search}
+          name="search"
+          onChange={props.handleInputChange}
+          type="text"
+          placeholder="Search"
+        />
+      </form>
     </div>
-    );
-  }
-  // }
+  );
+}
 
-
-// export default Form;
+// export default Form is not needed because we are exporting default function Form
