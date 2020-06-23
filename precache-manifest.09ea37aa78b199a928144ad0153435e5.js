202ef2f3e4bc0c88ea7c4f098272c5d799bf7785
Theresa E <t.eatherly@gmail.com>
2020-06-23 16:50:34 -0500

    Updates

---
+++ precache-manifest.09ea37aa78b199a928144ad0153435e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ba4d665e662a2703383f98058fda1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af8f6105c2c84370dec9",
+    "url": "/static/css/main.22bb9f12.chunk.css"
+  },
+  {
+    "revision": "39ac73b1f56b7a45ed26",
+    "url": "/static/js/2.56268e4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56268e4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8f6105c2c84370dec9",
+    "url": "/static/js/main.be4bc627.chunk.js"
+  },
+  {
+    "revision": "b18980a97af7a95bc0de",
+    "url": "/static/js/runtime-main.f18e0504.js"
+  }
+]);
