f6d1541fdb3ddf29b9bbfa479745fab21566b526
Theresa E <t.eatherly@gmail.com>
2020-06-25 11:01:53 -0500

    fixed jumbotron

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,17 +1,38 @@
 import React from "react";
 
- function Jumbotron() {
-   
+// We use JSX curly braces to evaluate the style object on the JSX tag
+
+function Jumbotron() {
   return (
-    <Jumbotron fluid>
-      <h1>Employee Directory!</h1>
-      <p>
-        Search the Employee Directory by typing in the first or last name in the
+      <div className="Jumbotron">
+    <h1>Employee Directory!</h1>
+    <p>
+      Search the Employee Directory by typing in the first or last name in the
         search bar. To Sort Employee Directory in alphabetical order, click on
-        the "Name" header in the table.
+         the "Name" header in the table.
       </p>
-    </Jumbotron>
-  );
+    </div>
+   
+   );
 }
 
 export default Jumbotron;
+
+
+
+
+
+
+// import React from "react";
+
+// export default function Navbar(props) {
+//   return (
+//     <>
+//     <Navbar expand="lg" variant="light" bg="light">     
+        
+//         </Navbar>
+//     </>
+//   );
+// }
+
+
